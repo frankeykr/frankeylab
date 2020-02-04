b70b3c36db6f2020c2ed0def5954e9c4e064078e
Francis <hyeongjin.kim@claves.co.jp>
2020-02-04 15:42:15 +0900

    page.php 수정

--- web/app/themes/frankeylab/page.php
+++ web/app/themes/frankeylab/page.php
@@ -3,7 +3,9 @@ get_header(); ?>
 
 <main id="main" class="page" role="main">
     <article class="page__content">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <?php the_content(); ?>
+    <?php endwhile; endif; ?>
     </article>
 </main>
 
