ca7513ef23471242c2d7ec19a7e0c2566d2519bb
Francis <hyeongjin.kim@claves.co.jp>
2020-02-05 15:50:23 +0900

    스타일 시트 수정, front-page.php 파일 작성

--- web/app/themes/frankeylab/archive.php
+++ web/app/themes/frankeylab/archive.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
             <div class="content-wrap">
@@ -102,5 +103,6 @@
             </div>
         </main><!-- .site-main -->
     </div><!-- .content-area -->
+    
 <?php
 get_footer();
---
+++ web/app/themes/frankeylab/front-page.php
@@ -0,0 +1,13 @@
+<?php
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <div class="content-wrap">
+                <?php the_content(); ?>
+            </div>
+        </main><!-- .site-main -->
+    </div><!-- .content-area -->
+
+<?php
+get_footer();
