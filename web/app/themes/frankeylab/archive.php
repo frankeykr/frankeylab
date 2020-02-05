ce2037578be56d15f08c1092ea93603d84dbf8fd
Francis <hyeongjin.kim@claves.co.jp>
2020-02-05 12:52:33 +0900

    arhcive.php 공통화

--- web/app/themes/frankeylab/archive.php
+++ web/app/themes/frankeylab/archive.php
@@ -1,23 +1,20 @@
 <?php get_header(); ?>
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
-            
-            <div class="wrapper">
+            <div class="content-wrap">
                 <div class="archive-page">
-                    <h1 class="archive-page-title">코드 아카이브</h1>
-                    
+                    <h1 class="archive-page-title"><?= get_post_type($post); ?></h1>
                     <div class="archive-page__selected-tag">
                         <?php
-                        $currentSelectedTagName = '전체';
-                        $currentSelectedTagSlug = get_query_var('code-tag');
+                        $postType = get_post_type($post);
+                        $currentSelectedTagName = 'ALL';
+                        $currentSelectedTagSlug = get_query_var($postType . '-tag');
                         if (!empty($currentSelectedTagSlug)) {
-                            $currentSelectedTagName = '#' . get_term_by('slug', $currentSelectedTagSlug, 'code-tag')->name;
+                            $currentSelectedTagName = '#' . get_term_by('slug', $currentSelectedTagSlug, $postType . '-tag')->name;
                         }
                         ?>
                         <p class="selected-tag"><span><?= $currentSelectedTagName; ?></span> ← 검색결과</p>
                     </div>
-                    
-
                     <div class="archive-page__tags">
                         <ul class="tag-list">
                             <?php
@@ -26,15 +23,15 @@
                                 $isTheTagSelected = 'current-selected-tag';
                             } ?>
                             <li class="tag-list__item <?= $isTheTagSelected; ?>">
-                                <a href="<?= get_post_type_archive_link('code'); ?>" class="tag-link">
-                                    <p>전체</p>
+                                <a href="<?= get_post_type_archive_link($postType); ?>" class="tag-link">
+                                    <p>ALL</p>
                                 </a>
                             </li>
                             <?php
-                            $allTags = get_terms('code-tag');
+                            $allTags = get_terms($postType . '-tag');
                             if ($allTags) {
                                 foreach ($allTags as $tag) :
-                                    $linkToTagLimitedArchivePage = get_post_type_archive_link('code') . '?code-tag=' . $tag->slug;
+                                    $linkToTagLimitedArchivePage = get_post_type_archive_link($postType) . '?' . $postType . '-tag=' . $tag->slug;
                                     $isTheTagSelected = '';
                                     if (urldecode($tag->slug) == $currentSelectedTagSlug) {
                                         $isTheTagSelected = 'current-selected-tag';
@@ -59,10 +56,8 @@
                                 while (have_posts()):the_post();
                                 $postImageUrl = "";
                                 if (has_post_thumbnail()) {
-                                    //アイキャッチ画像があればそのurlを返す
                                     $postImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large')[0];
                                 } else {
-                                    //アイキャッチ画像がない場合は代替画像のurlを返す
                                     $postImageUrl = get_stylesheet_directory_uri() . "/image/dummy.jpg";
                                 }
                                 ?>
@@ -71,8 +66,8 @@
                                             <div class="new-icon-container">
                                                 <?php
                                                 $today = date_i18n('U');
-                                                $worksPublishDay = get_the_time('U');
-                                                $dayDifference = ($today - $worksPublishDay) / 86400;
+                                                $postPublishDay = get_the_time('U');
+                                                $dayDifference = ($today - $postPublishDay) / 86400;
                                                 if ($dayDifference < 14) { ?>
                                                     <p class="new-icon">NEW!</p>
                                                 <?php } ?>
@@ -84,7 +79,7 @@
                                                 <h2 class="post-title"><?php the_title(); ?></h2>
                                                 <ul class="each-post-tags-container">
                                                     <?php
-                                                    $eachPostTags = get_the_terms($post->ID, 'code-tag');
+                                                    $eachPostTags = get_the_terms($post->ID, $postType . '-tag');
                                                     if ($eachPostTags) {
                                                         foreach ($eachPostTags as $tag) {
                                                             echo '<li class="each-post-tags-container__tag"><p>#' . esc_html($tag->name) . '</p></li>';
@@ -99,7 +94,7 @@
                             endif; ?>
                         </ul>
                         <?php
-                        $loadingImage = '<img class="archive-code__pagination-next__image" src="'. get_stylesheet_directory_uri() .'/image/loader-icon.gif">';
+                        $loadingImage = '<img class="archive-page__pagination-next__image" src="'. get_stylesheet_directory_uri() .'/image/loader-icon.gif">';
                         next_posts_link($loadingImage);
                         ?>
                     </div>
