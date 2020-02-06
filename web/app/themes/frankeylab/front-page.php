5518959c76971ecb381d0fbf4207d85f2516ad1e
Francis <hyeongjin.kim@claves.co.jp>
2020-02-06 15:59:52 +0900

    스타일 시트 수정 및 템플릿 수정

--- web/app/themes/frankeylab/front-page.php
+++ web/app/themes/frankeylab/front-page.php
@@ -13,8 +13,14 @@ get_header(); ?>
                             <img class="profile__image-container__image" 
                             src="<?= get_stylesheet_directory_uri()?>/image/profile.png" alt="profile-img">
                         </div>
+                        <div class="profile__name-container">
+                            <span class="profile__name-container__name">TEST</span>
+                        </div>
                         <div class="profile__content-container">
                             <p class="profile__content-container__content">
+                                블로그·어필리에이트·프로그래밍을 사랑합니다. 
+                                신규 졸업자로 세부 섬에 취직 → 11개월 만에 퇴직 → 프리랜서 → 창업 → 창업 실패 → 블로그를 쓰다 → 
+                                블로그 수익 7자릿수 달성. 평소에는 방콕을 중심으로 남국에 틀어박히면서 생활비는 5만 엔 정도로 살고 있습니다.
                             </p>
                         </div>
                         <div class="profile__link-container">
