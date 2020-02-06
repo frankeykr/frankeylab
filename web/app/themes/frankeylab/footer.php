4a98de213a515f0a6749ee76ee87f1aba391d084
hyeongjin kim <hyeongjin184@gmail.com>
2020-02-07 00:20:41 +0900

    instagram feed 플러그인 추가

--- web/app/themes/frankeylab/footer.php
+++ web/app/themes/frankeylab/footer.php
@@ -1,4 +1,7 @@
 <footer>
+    <div class="site-footer">
+        <?php echo do_shortcode('[instagram-feed]'); ?>
+    </div>
     <?php get_template_part( 'template-parts/footer/footer', 'widgets' ); ?>
 </footer>
 <?php wp_footer(); ?>
