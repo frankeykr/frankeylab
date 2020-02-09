82065f00d4e52c999ad3a5f198afc0f680e0e4d1
hyeongjin kim <hyeongjin184@gmail.com>
2020-02-09 23:37:58 +0900

    archive 수정

--- web/app/themes/frankeylab/archive.php
+++ web/app/themes/frankeylab/archive.php
@@ -3,17 +3,90 @@
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
             <div class="content-wrap">
-                <div class="archive-page">
+                <?php
+                $postType = get_post_type($post);
+                $currentSelectedTagName = 'ALL';
+                $currentSelectedTagSlug = get_query_var($postType . '-tag');
+                if (!empty($currentSelectedTagSlug)) {
+                    $currentSelectedTagName = '#' . get_term_by('slug', $currentSelectedTagSlug, $postType . '-tag')->name;
+                }
+                ?>
+                <div class="content archive-page">
                     <h1 class="archive-page-title"><?= get_post_type($post); ?></h1>
-                    <div class="archive-page__selected-tag">
+                    <div class="post archive-page__posts">
+                        <ul class="post-list">
+                            <?php
+                            if (have_posts()):
+                                while (have_posts()):the_post();
+                                $postImageUrl = "";
+                                if (has_post_thumbnail()) {
+                                    $postImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large')[0];
+                                } else {
+                                    $postImageUrl = get_stylesheet_directory_uri() . "/image/dummy.jpg";
+                                }
+                                ?>
+                                    <li class="post-list__item">
+                                        <div class="post-list__item__new-icon">
+                                            <?php
+                                            $today = date_i18n('U');
+                                            $postPublishDay = get_the_time('U');
+                                            $dayDifference = ($today - $postPublishDay) / 86400;
+                                            if ($dayDifference < 14) { ?>
+                                                <span class="new-icon">NEW!</span>
+                                            <?php } ?>
+                                        </div>
+                                        <div class="post-list__item__title">
+                                            <a href="<?php the_permalink(); ?>" class="link-to-single-page">
+                                                <h2 class="title"><?= wp_trim_words(get_the_title(), 52, '⋯'); ?></h2>
+                                            </a>
+                                        </div>
+                                        <div class="post-list__item__date">
+                                            <span class="date"><?php the_date('n/j'); ?></span>
+                                        </div>
+                                        <div class="post-list__item__image">
+                                            <div class="year-container">
+                                                <span class="year"><?php the_time('Y'); ?></span>
+                                            </div>
+                                            <div class="post-image-container">
+                                                <img class="image" src="<?= $postImageUrl; ?>"
+                                                    alt="<?php the_title(); ?>">
+                                            </div>
+                                            <ul class="tags-container">
+                                                <?php
+                                                $eachPostTags = get_the_terms($post->ID, $postType . '-tag');
+                                                if ($eachPostTags) {
+                                                    foreach ($eachPostTags as $tag) {
+                                                        echo '<li class="each-post-tags-container__tag"><p>#' . esc_html($tag->name) . '</p></li>';
+                                                    }
+                                                } ?>
+                                            </ul>
+                                        </div>
+                                        <div class="post-list__item__content">
+                                            <p class="content"><?= wp_trim_words(get_the_content(), 30, '⋯'); ?></p>
+                                            <a href="<?php the_permalink(); ?>" class="link">
+                                                <span>READ MORE</span>
+                                                <div class="arrow-container">
+                                                    <div class="arrow-box">
+                                                        <span class="arrow primera next"></span>
+                                                        <span class="arrow segunda next"></span>
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </li>
+                                <?php
+                                endwhile;
+                            endif; ?>
+                        </ul>
                         <?php
-                        $postType = get_post_type($post);
-                        $currentSelectedTagName = 'ALL';
-                        $currentSelectedTagSlug = get_query_var($postType . '-tag');
-                        if (!empty($currentSelectedTagSlug)) {
-                            $currentSelectedTagName = '#' . get_term_by('slug', $currentSelectedTagSlug, $postType . '-tag')->name;
-                        }
+                        $loadingImage = '<img class="archive-page__pagination-next__image" src="'. get_stylesheet_directory_uri() .'/image/loader-icon.gif">';
+                        next_posts_link($loadingImage);
                         ?>
+                    </div>
+                </div>
+
+                <aside class="sidebar">
+                    <div class="archive-page__selected-tag">
                         <p class="selected-tag"><span><?= $currentSelectedTagName; ?></span> ← 검색결과</p>
                     </div>
                     <div class="archive-page__tags">
@@ -49,57 +122,8 @@
                         </ul>
                     </div>
 
+                </aside>
 
-                    <div class="archive-page__posts">
-                        <ul class="post-list">
-                            <?php
-                            if (have_posts()):
-                                while (have_posts()):the_post();
-                                $postImageUrl = "";
-                                if (has_post_thumbnail()) {
-                                    $postImageUrl = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'large')[0];
-                                } else {
-                                    $postImageUrl = get_stylesheet_directory_uri() . "/image/dummy.jpg";
-                                }
-                                ?>
-                                    <li class="post-list__item">
-                                        <a href="<?php the_permalink(); ?>" class="link-to-single-page">
-                                            <div class="new-icon-container">
-                                                <?php
-                                                $today = date_i18n('U');
-                                                $postPublishDay = get_the_time('U');
-                                                $dayDifference = ($today - $postPublishDay) / 86400;
-                                                if ($dayDifference < 14) { ?>
-                                                    <p class="new-icon">NEW!</p>
-                                                <?php } ?>
-                                            </div>
-                                            <div class="post-image-container">
-                                                <img class="post-image-container__image" src="<?= $postImageUrl; ?>" alt="<?php the_title(); ?>">
-                                            </div>
-                                            <div class="post-title-and-tags">
-                                                <h2 class="post-title"><?php the_title(); ?></h2>
-                                                <ul class="each-post-tags-container">
-                                                    <?php
-                                                    $eachPostTags = get_the_terms($post->ID, $postType . '-tag');
-                                                    if ($eachPostTags) {
-                                                        foreach ($eachPostTags as $tag) {
-                                                            echo '<li class="each-post-tags-container__tag"><p>#' . esc_html($tag->name) . '</p></li>';
-                                                        }
-                                                    } ?>
-                                                </ul>
-                                            </div>
-                                        </a>
-                                    </li>
-                                <?php
-                                endwhile;
-                            endif; ?>
-                        </ul>
-                        <?php
-                        $loadingImage = '<img class="archive-page__pagination-next__image" src="'. get_stylesheet_directory_uri() .'/image/loader-icon.gif">';
-                        next_posts_link($loadingImage);
-                        ?>
-                    </div>
-                </div>
             </div>
         </main><!-- .site-main -->
     </div><!-- .content-area -->
