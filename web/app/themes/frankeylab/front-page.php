e4bd426e8bee71ba96c157b2211ca90dec7a2acc
frankeykr <frankeykr@gmail.com>
2022-02-07 21:19:45 +0900

    사이드바 광고추가

--- web/app/themes/frankeylab/front-page.php
+++ web/app/themes/frankeylab/front-page.php
@@ -11,6 +11,18 @@ get_header(); ?>
                 </article>
                 <aside class="sidebar front-page__sidebar">
                     <?php echo do_shortcode('[profile]'); ?>
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2546334153394027"
+                        crossorigin="anonymous"></script>
+                    <!-- 사이드바 -->
+                    <ins class="adsbygoogle"
+                        style="display:block"
+                        data-ad-client="ca-pub-2546334153394027"
+                        data-ad-slot="4689451836"
+                        data-ad-format="auto"
+                        data-full-width-responsive="true"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
                 </aside>
             </div>
         </main><!-- .site-main -->
