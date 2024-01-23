e345ebd2c44d9bd38417af5ea64ec64f8bdb053f
hyeongjin.kim <hyeongjin.kim@accenture.com>
2024-01-23 15:24:16 +0900

    광고 위치 변경

--- web/app/themes/frankeylab/single.php
+++ web/app/themes/frankeylab/single.php
@@ -49,19 +49,19 @@ if (have_posts()):
                     </div>
                 </div>
                 <div class="content-wrap single-page-wrap">
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2546334153394027"
-                        crossorigin="anonymous"></script>
-                    <!-- 본문위 -->
-                    <ins class="adsbygoogle"
-                        style="display:block"
-                        data-ad-client="ca-pub-2546334153394027"
-                        data-ad-slot="6848726175"
-                        data-ad-format="auto"
-                        data-full-width-responsive="true"></ins>
-                    <script>
-                        (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
                     <div class="single-page">
+                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2546334153394027"
+                            crossorigin="anonymous"></script>
+                        <!-- 본문위 -->
+                        <ins class="adsbygoogle"
+                            style="display:block"
+                            data-ad-client="ca-pub-2546334153394027"
+                            data-ad-slot="6848726175"
+                            data-ad-format="auto"
+                            data-full-width-responsive="true"></ins>
+                        <script>
+                            (adsbygoogle = window.adsbygoogle || []).push({});
+                        </script>
                         <div class="single-page__content">
                             <?php the_content(); ?>
                             <div class="share-btn">
