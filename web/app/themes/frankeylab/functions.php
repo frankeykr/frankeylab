19440a9aeb610db7061e3071c0620f6713c52a25
hyeongjin.kim <hyeongjin.kim@accenture.com>
2024-01-23 15:33:38 +0900

    프론트 피드 광고 삽입

--- web/app/themes/frankeylab/functions.php
+++ web/app/themes/frankeylab/functions.php
@@ -787,6 +787,17 @@ function add_japan_list_code_short_code() {
             ?>
         </ul>
     </section>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2546334153394027"
+        crossorigin="anonymous"></script>
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-format="fluid"
+        data-ad-layout-key="-53+d8+69-ev-7q"
+        data-ad-client="ca-pub-2546334153394027"
+        data-ad-slot="7611660429"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
     <?php
     return ob_get_clean();
 }
