af4ed2001578b6759f273b39bd845113c8823fc9
hyeongjin.kim <hyeongjin.kim@accenture.com>
2024-10-20 20:09:18 +0900

    사이드바 광고 고정기능 추가

--- web/app/themes/frankeylab/front-page.php
+++ web/app/themes/frankeylab/front-page.php
@@ -11,18 +11,22 @@ get_header(); ?>
                 </article>
                 <aside class="sidebar front-page__sidebar">
                     <?php echo do_shortcode('[profile]'); ?>
-                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2546334153394027"
-                        crossorigin="anonymous"></script>
-                    <!-- 사이드바 -->
-                    <ins class="adsbygoogle"
-                        style="display:block"
-                        data-ad-client="ca-pub-2546334153394027"
-                        data-ad-slot="4689451836"
-                        data-ad-format="auto"
-                        data-full-width-responsive="true"></ins>
-                    <script>
-                        (adsbygoogle = window.adsbygoogle || []).push({});
-                    </script>
+                    <div class="sidebar-ad">
+                        <div class="ad-container">
+                            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2546334153394027"
+                                crossorigin="anonymous"></script>
+                            <!-- 사이드바 -->
+                            <ins class="adsbygoogle"
+                                style="display:block"
+                                data-ad-client="ca-pub-2546334153394027"
+                                data-ad-slot="4689451836"
+                                data-ad-format="auto"
+                                data-full-width-responsive="true"></ins>
+                            <script>
+                                (adsbygoogle = window.adsbygoogle || []).push({});
+                            </script>
+                        </div>
+                    </div>
                 </aside>
             </div>
             <div class="content-bottom-ads">
@@ -42,5 +46,18 @@ get_header(); ?>
         </main><!-- .site-main -->
     </div><!-- .content-area -->
 
+    <!-- 사이드바 광고 Sticky -->
+    <script>
+        const adContainer = document.querySelector(".ad-container");
+        const sidebar = document.querySelector(".sidebar-ad");
+        const offsetTop = sidebar.offsetTop;
+        window.addEventListener("scroll", function () {
+            if (window.pageYOffset > offsetTop) {
+            adContainer.classList.add("ad-fixed");
+            } else {
+            adContainer.classList.remove("ad-fixed");
+            }
+        });
+    </script>
 <?php
 get_footer();
